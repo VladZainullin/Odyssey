34d52f97ceb137f175129f98388bdceae89d4362
Vladislav Zainullin <mittoface02@mail.ru>
2023-11-04 22:31:08 +0300

    Добавляет сервис работы с jwt token

---
+++ Identification/JwtTokenService.cs
@@ -0,0 +1,37 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Identification;
+
+public sealed class JwtTokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes("fwefwef");
+        
+        var claims = new Claim[]
+        {
+            new(JwtRegisteredClaimNames.Jti, user.Id.ToString()),
+        };
+
+        var tokenDescription = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTimeOffset.Now.AddMinutes(5).DateTime,
+            Issuer = "",
+            Audience = "",
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), 
+                SecurityAlgorithms.Sha256)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescription);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
