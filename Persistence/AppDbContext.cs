70e50396de1e0babac7dc32fff643a8fa76063d9
Vladislav Zainullin <mittoface02@mail.ru>
2023-11-05 16:43:14 +0300

    Добавляет сущности в контекст

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -1,4 +1,4 @@
-using Contracts.Persistence;
+using System.Reflection;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,4 +11,17 @@ internal sealed class AppDbContext : DbContext
     }
 
     public DbSet<User> Users => Set<User>();
+
+    public DbSet<Note> Notes => Set<Note>();
+
+    public DbSet<Label> Labels => Set<Label>();
+
+    public DbSet<NoteLabel> NoteLabels => Set<NoteLabel>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        base.OnModelCreating(modelBuilder);
+    }
 }
