1ce2091bbfb7ec6a5b85c1bab994d304ea95d99a
Vladislav Zainullin <mittoface02@mail.ru>
2023-11-05 14:58:26 +0300

    Подключает пакет MediatR к сборке Application

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -6,6 +7,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(c =>
+        {
+            c.RegisterServicesFromAssembly(Assembly.GetCallingAssembly());
+        });
+        
         return services;
     }
 }
