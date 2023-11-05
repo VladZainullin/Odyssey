bb4dc46d1a8e0922e9adfa7ec647ccce7d3f64ed
Vladislav Zainullin <mittoface02@mail.ru>
2023-11-05 17:34:52 +0300

    Добавляет миграции

---
+++ Persistence/Migrations/20231105143416_Init.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    username = table.Column<string>(type: "TEXT", nullable: false),
+                    password_hash = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "labels",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    user_id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_labels", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_labels_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "notes",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    header = table.Column<string>(type: "TEXT", nullable: false),
+                    body = table.Column<string>(type: "TEXT", nullable: false),
+                    user_id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_notes", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_notes_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "note_labels",
+                columns: table => new
+                {
+                    note_id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    label_id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_note_labels", x => new { x.note_id, x.label_id });
+                    table.ForeignKey(
+                        name: "fk_note_labels_labels_label_id",
+                        column: x => x.label_id,
+                        principalTable: "labels",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_note_labels_notes_note_id",
+                        column: x => x.note_id,
+                        principalTable: "notes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_labels_user_id",
+                table: "labels",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_note_labels_label_id",
+                table: "note_labels",
+                column: "label_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_notes_user_id",
+                table: "notes",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "note_labels");
+
+            migrationBuilder.DropTable(
+                name: "labels");
+
+            migrationBuilder.DropTable(
+                name: "notes");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
